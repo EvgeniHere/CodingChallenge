f84154bd846c0313240064774fac2d18cb24a47c
EvgeniUschakov <evgeni.uschakov@gmail.com>
2018-09-29 19:36:23 +0200

    Firstbot edited to normal opponent

--- FirstBot.java
+++ FirstBot.java
@@ -3,10 +3,18 @@ import java.util.List;
 public class FirstBot extends Bot {
    
     public FirstBot(int anzahlKarten) {
-        super(anzahlKarten, "EvgenisBot");
+        super(anzahlKarten, "FirstBot");
     }
     
     public void legeKarte(List<Karte> gegnerKarten, Karte spielfeldKarte) {
         //place Your Code here (you can find an example in SecondBot.java)
+        if (!gelegt) {
+            for (int i = 0; i < karten.size(); i++) {
+                if (karten.get(i).getWert() == spielfeldKarte.getWert() || karten.get(i).getFarbe() == spielfeldKarte.getFarbe())  {
+                    this.legeKarte(i);
+                    break;
+                }
+            }
+        }
     }
 }
